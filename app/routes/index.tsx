ceea73661866387678d68f8d86ad91f84e089460
seven <7b37b3@gmail.com>
2022-11-17 18:25:01 -0300

    added getUserSession, getUserId, requiredUserId - force login on index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,30 @@
+import { redirect, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { Link } from "react-router-dom";
+import { getUserId } from "../utils/session.server"
+
+export async function loader({ request }) {
+  const session = await getUserId(request);
+  if (!session) {
+    return redirect("/login")
+  }
+
+  return session;
+}
 
 export default function Index() {
+  const currentUser = useLoaderData();
   return (
-    <div>
-      <h1 >
-        Hello world!
-      </h1>
-      <Link to="/posts" >
-        Posts
-      </Link>
+    <div className="min-h-screen flex justify-center items-center">
+      <div>
+        <h1>
+          Hello {currentUser}!
+        </h1>
+        <Link to="/posts" >
+          Posts
+        </Link>
+      </div>
+
     </div>
   );
 }
